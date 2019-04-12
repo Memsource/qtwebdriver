8adcf86d19c2bb8e95719fa3dfcfe85003c0de77
Michal Lazo <michal.lazo@memsource.com>
2019-04-12 09:58:31 +0200

    fix support for qt 5.12.3

--- src/webdriver/extension_qt/widget_view_executor.cc
+++ src/webdriver/extension_qt/widget_view_executor.cc
@@ -76,6 +76,8 @@
 #include <QtGui/5.12.1/QtGui/qpa/qwindowsysteminterface.h>
 #elif (QT_VERSION == QT_VERSION_CHECK(5, 12, 2))
 #include <QtGui/5.12.2/QtGui/qpa/qwindowsysteminterface.h>
+#elif (QT_VERSION == QT_VERSION_CHECK(5, 12, 3))
+#include <QtGui/5.12.3/QtGui/qpa/qwindowsysteminterface.h>
 #else
 #include <QtGui/5.13.0/QtGui/qpa/qwindowsysteminterface.h>
 #endif  
